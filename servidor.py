6c009f57f944573c81572d8c0f701a33ff19c61d
jucaso23 <sozaj11@gmail.com>
2025-04-24 01:50:25 -0600

    fix: mover proyecto a la raíz del repo

--- servidor.py
+++ servidor.py
@@ -60,7 +60,7 @@ def obtener_ubicacion():
         print("[✘] Error:", e)
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
 
 
